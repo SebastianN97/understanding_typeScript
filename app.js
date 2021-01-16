bbd35cc19bec728247abcbec0e26b44648ec8e1c
SebastianN97 <sebbe132132@gmail.com>
2021-01-16 21:39:25 +0100

    compiling ts file into js file.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+add(number1, number2, printResult);
--- app.ts
+++ app.ts
@@ -10,5 +10,4 @@ const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 
-const result = add(number1, number2, printResult); 
-console.log(result);
+add(number1, number2, printResult); 
