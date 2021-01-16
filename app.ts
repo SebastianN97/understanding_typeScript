374e97f65ed98e301e901e79285814141ab05483
SebastianN97 <sebbe132132@gmail.com>
2021-01-16 21:43:59 +0100

    Adding phrase with types string and modifying calculation to result.

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+
+  const result = n1 + n2;
+
   if (showResult) {
-    console.log(n1 + n2)
+    console.log(phrase + result)
   } else {
-    return n1 + n2;
+    return result;
   }
 }
 
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = "Result is: "
 
-add(number1, number2, printResult); 
+add(number1, number2, printResult, resultPhrase); 
