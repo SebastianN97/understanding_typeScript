2e5a135a7e0a03572fef6c499357d3fb2a582b0e
SebastianN97 <sebbe132132@gmail.com>
2021-01-18 20:28:08 +0100

    Working with Tuple Types.

--- app.ts
+++ app.ts
@@ -46,3 +46,18 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby);
 }
+
+
+// Tuples
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // Tuple
+} = {
+  name: "Sebastian",
+  age: 23,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"] //Tuple
+};
