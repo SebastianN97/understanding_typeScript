4aa5228607e78361c650aa35cff5eef86c35dc4b
SebastianN97 <sebbe132132@gmail.com>
2021-01-19 19:31:54 +0100

    Writing union types.

--- app.ts
+++ app.ts
@@ -76,3 +76,16 @@ const person3: {
   hobbies: ["Sports", "Cooking"],
   role: Role.ADMIN
 };
+
+
+// Union 
+
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") [
+    result = input1 + input2;
+  ] else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
